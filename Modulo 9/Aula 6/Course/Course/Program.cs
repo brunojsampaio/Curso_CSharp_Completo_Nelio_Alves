0eb39918e836b13e477b235567b3c01b6714d848
Bruno Sampaio <brunojsampaio@gmail.com>
2021-03-28 09:08:54 -0300

    Modulo 9 Aula 8

--- Modulo 9/Aula 6/Course/Course/Program.cs
+++ Modulo 9/Aula 6/Course/Course/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Course.Entities.Enums;
+using Course.Entities;
+using System;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,7 +9,44 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter department's name: ");
+            string departmentName = Console.ReadLine();
+            Console.WriteLine("Enter worker data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department department = new Department(departmentName);
+            Worker worker = new Worker(name, level, baseSalary, department);
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Enter #{i} contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours): ");
+                int duration = int.Parse(Console.ReadLine());
+
+                worker.AddContract(new HourContract(date, valuePerHour, duration));
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3, 4));
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Department: {worker.Department.Name}");
+            Console.WriteLine($"Income for {month}/{year}: {worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
