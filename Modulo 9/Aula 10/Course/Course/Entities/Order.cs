3d2c06df750858cbd090a5a00d47801495d0b1ec
Bruno Sampaio <brunojsampaio@gmail.com>
2021-03-29 08:19:00 -0300

    Modulo 9 Aula 10

---
+++ Modulo 9/Aula 10/Course/Course/Entities/Order.cs
@@ -0,0 +1,67 @@
+﻿using Course.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Course.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus orderStatus, Client client)
+        {
+            Moment = moment;
+            OrderStatus = orderStatus;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            OrderItems.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            OrderItems.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in OrderItems)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Order moment: {Moment.ToString("dd/MM/yyyy HH:mm:ss")}");
+            stringBuilder.AppendLine($"Order status: {OrderStatus}");
+            stringBuilder.AppendLine($"Client: {Client}");
+            stringBuilder.AppendLine("Order items:");
+            double sum = 0;
+            foreach (OrderItem orderItem in OrderItems)
+            {
+                stringBuilder.AppendLine(orderItem.ToString());
+                sum += orderItem.SubTotal();
+            }
+            stringBuilder.AppendLine($"Total price: ${sum.ToString("F2", CultureInfo.InvariantCulture)}");
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Modulo 9/Aula 10/Course/Course/Program.cs
@@ -0,0 +1,55 @@
+﻿using Course.Entities;
+using Course.Entities.Enums;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string clientName = Console.ReadLine();
+            Console.Write("Email: ");
+            string clientEmail = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime clientBirthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(
+                DateTime.Now, 
+                orderStatus, 
+                new Client(clientName, clientEmail, clientBirthDate));
+
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                order.AddItem(
+                    new OrderItem(
+                        quantity, 
+                        productPrice, 
+                        new Product(productName, productPrice)));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ORDER SUMMARY:");
+            Console.WriteLine(order);
+        }
+    }
+}
