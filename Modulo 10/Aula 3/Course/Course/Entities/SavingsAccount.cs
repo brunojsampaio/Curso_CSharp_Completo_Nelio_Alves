2202643a86addf67c7b3df116820194ada4403ce
Bruno Sampaio <brunojsampaio@gmail.com>
2021-03-31 09:52:18 -0300

    Modulo 10 Aula 6

--- Modulo 10/Aula 3/Course/Course/Entities/SavingsAccount.cs
+++ Modulo 10/Aula 3/Course/Course/Entities/SavingsAccount.cs
@@ -19,7 +19,7 @@
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount)
+        public sealed override void Withdraw(double amount)
         {
             base.Withdraw(amount);
 
