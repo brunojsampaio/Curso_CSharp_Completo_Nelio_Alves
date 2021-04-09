14a57879598858f7050a4b4e1094fcc551fe7e9a
Bruno Sampaio <brunojsampaio@gmail.com>
2021-04-09 08:50:18 -0300

    Modulo 14 Aula 7

---
+++ Modulo 14/Aula 7/Course/Course/Services/PayPalService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services
+{
+    class PaypalService : IOnlinePaymentService
+    {
+        private const double _feePercentage = 0.02;
+        private const double _monthlyInterest = 0.01;
+
+        public double Interest(double amount, int months)
+        {
+            return amount * _monthlyInterest * months;
+        }
+
+        public double PaymentFee(double amount)
+        {
+            return amount * _feePercentage;
+        }
+    }
+}
