582c4e34666f427206baa54bee7f03bd66c7fcc5
Bruno Sampaio <brunojsampaio@gmail.com>
2021-04-11 10:10:22 -0300

    Modulo 14 Aula 9

---
+++ Modulo 14/Aula 9/Course/Course/Devices/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course.Devices
+{
+    class Printer : Device, IPrinter
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Printer processing: " + document);
+        }
+
+        public void Print(string document)
+        {
+            Console.WriteLine("Printer print " + document);
+        }
+    }
+}
---
+++ Modulo 14/Aula 9/Course/Course/Devices/Scanner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course.Devices
+{
+    class Scanner : Device, IScanner
+    {
+        public override void ProcessDoc(string document)
+        {
+            Console.WriteLine("Scanner processing: " + document);
+        }
+
+        public string Scan()
+        {
+            return "Scanner scan result";
+        }
+    }
+}
