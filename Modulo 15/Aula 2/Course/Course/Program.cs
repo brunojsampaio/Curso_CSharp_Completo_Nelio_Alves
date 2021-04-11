01ff02ae23ee740ee6048d8aa7bd5e7f2a069598
Bruno Sampaio <brunojsampaio@gmail.com>
2021-04-11 10:52:15 -0300

    Modulo 15 Aula 2

---
+++ Modulo 15/Aula 2/Course/Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 4, 11, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            String s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
