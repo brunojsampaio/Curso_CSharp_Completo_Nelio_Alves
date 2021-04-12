0d454348f3fdb01bc5e91de9cc12b22fbc021b4b
Bruno Sampaio <brunojsampaio@gmail.com>
2021-04-12 19:05:09 -0300

    Modulo 16 Aula 6

--- Modulo 16/Aula 2/Course/Course/Program.cs
+++ Modulo 16/Aula 2/Course/Course/Program.cs
@@ -21,6 +21,17 @@ namespace Course
             {
                 Console.WriteLine(p);
             }
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
         }
 
         static int CompareProducts(Product p1, Product p2)
